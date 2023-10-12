ba5101d18b68c1196cfe3363dc3ca05a8d9f9ae2
Si Thu Win <winst@corpit.com.sg>
2023-10-12 22:06:18 +0630

    responsive changes

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -78,75 +78,68 @@ export default function AboutMeComponent() {
   }, []);
   return (
     <>
-      <div className="hover:text-blue-700 transition duration-1000 ease-in-out">
-        <div className="container mx-auto grid grid-cols-2 gap-2 px-28 pt-32 text-white">
-          <div>
-            <img
-              className="rounded-xl"
-              src="images/logos/about-me.jpg"
-              title=""
-              alt=""
-            />
-          </div>
-          <div className=" px-10">
-            <h3 className="font-light text-3xl  py-3 tracking-widest">
-              ABOUT ME.
-            </h3>
-            <h3 className="text-3xl  font-bold py-3">
-              I'm a Software Developer with over 5 years of experience.
-            </h3>
+      <div className="container mx-auto grid grid-cols-1 px-10 text-white lg:grid-cols-2 lg:gap-8 lg:pt-32 lg:px-28">
+        <div>
+          <img
+            className="rounded-xl"
+            src="images/logos/about-me.jpg"
+            title=""
+            alt=""
+          />
+        </div>
+        <div className="">
+          <h3 className="font-light text-3xl  py-3 tracking-widest">
+            ABOUT ME.
+          </h3>
+          <h3 className="text-3xl  font-bold py-3">
+            I'm a Software Developer with over 5 years of experience.
+          </h3>
 
-            <p className="text-slate-400	 text-justify py-5 ">
-              I'm a Software Developer with over 5 years of experience. I'm from
-              Myanmar. I code and create web elements for amazing people around
-              the world. I like to work with new people. New people new
-              Experiences.
-            </p>
-            <Link to="contactme" smooth={true} duration={800}>
-              <button className="mt-3 transform rounded-md bg-indigo-500   p-3 hover:bg-violet-600 transition duration-500 hover:scale-90">
-                Contact Me
-              </button>
-            </Link>
-          </div>
+          <p className="text-slate-400	 text-justify py-5 ">
+            I'm a Software Developer with over 5 years of experience. I'm from
+            Myanmar. I code and create web elements for amazing people around
+            the world. I like to work with new people. New people new
+            Experiences.
+          </p>
+          <Link to="contactme" smooth={true} duration={800}>
+            <button className="mt-3 transform rounded-md bg-indigo-500   p-3 hover:bg-violet-600 transition duration-500 hover:scale-90">
+              Contact Me
+            </button>
+          </Link>
+        </div>
+      </div>
+
+      <div className="container mx-auto grid grid-cols-1 px-10 text-white lg:grid-cols-3 lg:gap-8 lg:pt-32 lg:px-28">
+        <hr className="opacity-20 mb-10 col-span-3" />
+        <h3 className="font-light tracking-widest text-3xl mb-10 col-span-3">
+          EDUCATION & SKILLS
+        </h3>
+        <div className="Education  col-span-3 lg:col-span-1">
+          <EducationComponent />
+        </div>
+        <div className="skill  mx-5 col-span-3 lg:col-span-2">
+          <h3 className="font-bold text-2xl"> My skills</h3>
+          <p className="text-slate-400	 text-justify py-5 ">
+            I'm a Software Developer with over 5 years of experience. I code and
+            create web elements for amazing people around the world. I like to
+            work with new people. New people new Experiences.
+          </p>
+          {isMounted && (
+            <div className="grid grid-rows-6 grid-flow-col gap-2">
+              {skills.map((s, index) => (
+                <div key={index} className="flex items-center">
+                  <h3 className="font-bold pr-2  basis-2/4">{s.displayName}</h3>
+                  <StarRatingComponent skill={s.skill} className="basis-2/4" />
+                </div>
+              ))}
+            </div>
+          )}
         </div>
 
-        <div className="container mx-auto grid grid-cols-3 gap-3 px-28 pt-32 text-white">
-          <hr className="col-span-3 opacity-20 mb-10" />
-          <h3 className="font-light tracking-widest text-3xl col-span-3 mb-10">
-            EDUCATION & SKILLS
-          </h3>
-          <div className="Education mr-14">
-            <EducationComponent />
-          </div>
-          <div className="skill col-span-2 mx-5">
-            <h3 className="font-bold text-2xl"> My skills</h3>
-            <p className="text-slate-400	 text-justify py-5 ">
-              I'm a Software Developer with over 5 years of experience. I code
-              and create web elements for amazing people around the world. I
-              like to work with new people. New people new Experiences.
-            </p>
-            {isMounted && (
-              <div className="grid grid-rows-6 grid-flow-col gap-2">
-                {skills.map((s, index) => (
-                  <div key={index} className="flex items-center">
-                    <h3 className="font-bold pr-2  basis-2/4">
-                      {s.displayName}
-                    </h3>
-                    <StarRatingComponent
-                      skill={s.skill}
-                      className="basis-2/4"
-                    />
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
+        <hr className="col-span-3 opacity-20 my-10" />
 
-          <hr className="col-span-3 opacity-20 my-10" />
-
-          <div className="col-span-3">
-            <ExperienceComponent />
-          </div>
+        <div className="col-span-3">
+          <ExperienceComponent />
         </div>
       </div>
     </>
